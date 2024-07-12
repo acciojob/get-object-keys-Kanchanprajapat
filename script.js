f42d7fe317e002a2f403e43a68677ac92b702067
Kanchanprajapat <141110309+Kanchanprajapat@users.noreply.github.com>
2024-07-12 12:27:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //your JS code here. If required.
-let student {
+let student = {
 	name : "kanchan",
 }
 Object.prototype.getKeys = function() {
     return Object.keys(this);
 };
+console.log(student.getKeys());
